b6a39bf8e2aa06c0ad20090091e7e1c5f08bf2d5
yurley sanchez <yurley.sanchez@yurleys-MacBook-Pro.local>
2021-04-15 18:07:29 -0500

    add module admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Student, Teacher, Subject, Enrollment, Note
 
 # Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    list_filter = ('first_name', 'last_name')
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):  
+    list_display = ('first_name', 'last_name')
+    list_filter = ('first_name', 'last_name')
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin): 
+    list_display = ('name',)
+    list_filter = ('name',)
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin): 
+    pass
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin): 
+    pass
--- myapp/models.py
+++ myapp/models.py
@@ -5,16 +5,22 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-  
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
     
 class Teacher(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
 
 class Subject(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return '{}'.format(self.name)
 
 class Enrollment(models.Model):
     student = models.ForeignKey('Student', on_delete=models.CASCADE)
@@ -22,9 +28,15 @@ class Enrollment(models.Model):
     subject = models.ForeignKey('Subject', on_delete=models.CASCADE)
 
 
+    def __str__(self):
+        return '{} - {} - {}'.format(self.student, self.teacher, self.subject)
+
 class Note(models.Model):
     value = models.DecimalField(max_digits=5, decimal_places=2)
     enrollment = models.ForeignKey('Enrollment', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} - {}'.format(self.enrollment, self.value)
 
 
 
